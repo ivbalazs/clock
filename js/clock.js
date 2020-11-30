26e00e17b70ce03583929762b2bd2807852ff94e
En <ivbalazs@gmail.com>
2020-11-30 16:31:49 +0100

    Initial commit

---
+++ js/clock.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function currentTime() {
+    let date = new Date(); /* creating object of Date class */
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    hour = updateTime(hour);
+    min = updateTime(min);
+    sec = updateTime(sec);
+    document.getElementById("clock").innerText = hour + " : " + min + " : " + sec; /* adding time to the div */
+      let t = setTimeout(function(){ currentTime() }, 1000); /* setting timer */
+  }
+  
+  function updateTime(k) {
+    if (k < 10) {
+      return "0" + k;
+    }
+    else {
+      return k;
+    }
+  }
+  
+  currentTime(); /* calling currentTime() function to initiate the process */
+
